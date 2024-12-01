dbb08243d63e1e1372bc449d05408ebea52d1118
Onurkan <onurkancuha@hotmail.com>
2024-12-02 00:05:28 +0300

    he
    he

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.PROD 
-  ? 'https://chat-zv6y.onrender.com'
+  ? 'https://chat-zltm.onrender.com'
   : 'http://localhost:3001';
 
 export const socket = io(SOCKET_URL, {
